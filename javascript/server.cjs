06125b074ec7df481caf35ae2c0133e9cac0a969
jeudyr <fabian09_jeudy@hotmail.com>
2025-03-25 01:46:53 -0600

    Corregir rutas de CSS y JS

--- javascript/server.cjs
+++ javascript/server.cjs
@@ -30,7 +30,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  connectTimeout: 10000,  // 10 segundos de tiempo de espera para la conexión
+  connectTimeout: 1000,  // 10 segundos de tiempo de espera para la conexión
   authPlugins: {
     mysql_native_password: () => require('mysql2/lib/auth/mysql_native_password')
   }
