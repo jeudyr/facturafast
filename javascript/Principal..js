f1973a8f67103f3344be9856de89d8b94ee70d06
jeudyr <fabian09_jeudy@hotmail.com>
2025-04-06 13:46:00 -0600

    cambio

--- javascript/Principal..js
+++ javascript/Principal..js
@@ -129,7 +129,7 @@ document.getElementById('productForm').addEventListener('submit', function(event
     let descripcion = document.getElementById('productDescription').value;
     let tipo = document.getElementById('productType').value;
     let cantidad = parseInt(document.getElementById('productQuantity').value);
-    if(tipo!="servicio"){
+    if(tipo=="servicio"){
         cantidad=1;
     }
     let precio = parseFloat(document.getElementById('productPrice').value);
